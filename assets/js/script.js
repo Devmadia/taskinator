fe42a0cff03047165d6ea791bcee0281aa7cf3ec
Devmadia <agathaphan@icloud.com>
2020-06-10 14:59:38 -0500

    added TaskHandler

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,16 @@
 var buttonEl = document.querySelector("#save-task");
 var taskToDoEl = document.querySelector("#tasks-to-do");
-
-buttonEl.addEventListener("click", function() {
-    var listItemEl = document.createElement("li"); 
-    listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
-    taskToDoEl.appendChild(listItemEl);
-});
-
 var createTaskHandler = function() {
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     taskToDoEl.appendChild(listItemEl);
 }
+
+buttonEl.addEventListener("click", createTaskHandler) {
+    var listItemEl = document.createElement("li"); 
+    listItemEl.className = "task-item";
+    listItemEl.textContent = "This is a new task.";
+    taskToDoEl.appendChild(listItemEl);
+};
+
